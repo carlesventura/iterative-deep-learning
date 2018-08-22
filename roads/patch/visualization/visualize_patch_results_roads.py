44dce63fa9011464d5d4865393085362d99c8135
carlesventura <7792351+carlesventura@users.noreply.github.com>
2018-08-22 14:45:27 +0200

    Update visualize_patch_results_roads.py

--- roads/patch/visualization/visualize_patch_results_roads.py
+++ roads/patch/visualization/visualize_patch_results_roads.py
@@ -5,24 +5,16 @@ from astropy.stats import sigma_clipped_stats
 from photutils import find_peaks
 import numpy as np
 
+gt_base_dir = './results_dir/val_gt/'
+results_base_dir = './results_dir/val_results/'
+
 num_images = 14
 num_patches = 50
 start_img = 1
 patch_size = 64
 
-#peak_th = 100
-
-gt_base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/roads/Iterative_margin_6/val_gt/'
-results_base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/roads/Iterative_margin_6_200_epoches/val_results/epoch_130/'
-
-
-#idx_patch = 1
-
-
 for idx_patch in range(1,num_patches+1):
     for idx in range(start_img,start_img+num_images):
-
-        print(idx)
 
         road_img = Image.open(gt_base_dir + 'img_%02d_patch_%02d_img.png' %(idx, idx_patch))
         plt.imshow(road_img)
@@ -43,16 +35,4 @@ for idx_patch in range(1,num_patches+1):
         plt.scatter(pos_x_vector, pos_y_vector, marker='+', color='red', s=100, linewidth=3)
         plt.axis('off')
         plt.show()
-        #plt.savefig('/scratch_net/boxy/carlesv/HourGlasses_experiments/roads/Iterative_margin_6_200_epoches/val_results_paper/img_%02d_patch_%02d.png' %(idx, idx_patch), bbox_inches='tight')
-        #plt.close()
-        #axes[2,config_id].plot(sources['x_peak'], sources['y_peak'], ls='none', color='red',marker='+', ms=10, lw=1.5)
-
-
-
-
-
-
-
-
-
 
