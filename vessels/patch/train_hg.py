a7a1bdf5f73eee60fb99e39886cbe33e0817ab0c
carlesventura <7792351+carlesventura@users.noreply.github.com>
2018-08-22 13:18:46 +0200

    Update train_hg.py

--- vessels/patch/train_hg.py
+++ vessels/patch/train_hg.py
@@ -13,6 +13,11 @@ import timeit
 import scipy.misc
 
 # Setting of parameters
+# db_root_dir and save_dir parameters should be changed by the user
+
+db_root_dir = './gt_dbs/DRIVE/' #Directory where DRIVE dataset is stored
+save_dir = './results_dir/' #Directory where the patch model is saved during training
+
 # Parameters in p are used for the name of the model
 p = {}
 p['useRandom'] = 1  # Shuffle Images
@@ -25,9 +30,10 @@ p['numHG'] = 2  # Number of Stacked Hourglasses
 p['Block'] = 'ConvBlock'  # Select: 'ConvBlock', 'BasicBlock', 'BottleNeck', 'BottleneckPreact'
 p['GTmasks'] = 0 # Use GT Vessel Segmentations as input instead of Retinal Images
 
-junctions = True
+#Following default parameters do not differentiate veins from arteries
+junctions = False
 connected = True
-from_same_vessel = True
+from_same_vessel = False
 bifurcations_allowed = False
 save_vertices_indxs = False
 
@@ -37,12 +43,7 @@ numHGScales = 4  # How many times to downsample inside each HourGlass
 useTest = 1  # See evolution of the test set when training?
 testBatch = 1  # Testing Batch
 nTestInterval = 10  # Run on test set every nTestInterval iterations
-#seq_names = ('1-left', '3-left', '2-left')
-db_root_dir = '/scratch_net/boxy/carlesv/gt_dbs/DRIVE/'
-save_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/'
-vis_net = 0  # Visualize your network?
 gpu_id = int(os.environ['SGE_GPU'])  # Select which GPU, -1 if CPU
-#gpu_id = -1
 snapshot = 200  # Store a model every snapshot epochs
 
 
@@ -57,15 +58,11 @@ criterion = nn.MSELoss(size_average=True)
 
 # Use the following optimizer
 optimizer = optim.RMSprop(net.parameters(), lr=0.00005, alpha=0.99, momentum=0.0)
-# optimizer = optim.SGD(net.parameters(), lr=0.01/1000, momentum=0.1)
-# optimizer = optim.Adam(net.parameters(), lr=0.0005)
 
 
 # Preparation of the data loaders
 # Define augmentation transformations as a composition
-#composed_transforms = transforms.Compose([tb.ScaleNRotate(rots=(-30, 30), scales=(.75, 1.25)), tb.RandomHorizontalFlip(), tb.ToTensor()])
 if p['useAug'] == 1:
-    #composed_transforms = transforms.Compose([tb.ScaleNRotate(rots=(-90, 90), scales=(.5, 1.5)), tb.ToTensor(), tb.normalize()])
     composed_transforms = transforms.Compose([tb.ScaleNRotate(rots=(-30, 30), scales=(.75, 1.25)), tb.ToTensor()])
 else:
     composed_transforms = tb.ToTensor()
@@ -163,28 +160,6 @@ for epoch in range(0, nEpochs):
 
                 output = net.forward(inputs)
 
-                if ii == 0:
-                    pred = np.squeeze(np.transpose(output[len(output)-1].cpu().data.numpy()[0, :, :, :], (1, 2, 0)))
-
-                    if junctions:
-                        base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/tmp_results_junctions/'
-                    else:
-                        if not connected:
-                            base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/tmp_results_not_connected/'
-                        else:
-                            if from_same_vessel:
-                                if bifurcations_allowed:
-                                    base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/tmp_results_connected_same_vessel/'
-                                else:
-                                    base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/tmp_results_connected_same_vessel_wo_bifurcations/'
-                            else:
-                                base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/tmp_results_connected/'
-
-                    scipy.misc.imsave(base_dir + 'epoch_' + str(epoch) + '_test.png', pred)
-                    scipy.misc.imsave(base_dir + 'epoch_' + str(epoch) + '_gt.png', np.squeeze(gt.numpy()))
-                    np.save(base_dir + 'epoch_' + str(epoch) + '_gt.npy', np.squeeze(gt.numpy()))
-                    scipy.misc.imsave(base_dir + 'epoch_' + str(epoch) + '_img.png', np.transpose(np.squeeze(img.numpy()),(1,2,0)))
-
                 losses_test = [None] * p['numHG']
                 for i in range(0, len(outputs)):
                     losses_test[i] = criterion(outputs[i], gts)
@@ -197,63 +172,3 @@ for epoch in range(0, nEpochs):
                     running_loss_ts = 0
 
 
-# Separate interactive plotting of test set results
-# plt.close("all")
-# plt.ion()
-# f, ax_arr = plt.subplots(1, 3)
-# for ii, sample_batched in enumerate(testloader):
-#     img, gt = sample_batched['image'], sample_batched['gt']
-#     inputs = img / 255 - 0.5
-#
-#     # Forward pass of the mini-batch
-#     inputs = Variable(inputs)
-#     if gpu_id >= 0:
-#         inputs = inputs.cuda()
-#
-#     output = net.forward(inputs)
-#     for jj in range(int(img.size()[0])):
-#         pred = np.transpose(output[len(output)-1].cpu().data.numpy()[jj, :, :, :], (1, 2, 0))
-#         img_ = np.transpose(img.numpy()[jj, :, :, :], (1, 2, 0))
-#         gt_ = np.transpose(gt.numpy()[jj, :, :, :], (1, 2, 0))
-#         # Plot the particular example
-#         ax_arr[0].cla()
-#         ax_arr[1].cla()
-#         ax_arr[2].cla()
-#         ax_arr[0].set_title("Input Image")
-#         ax_arr[1].set_title("Ground Truth")
-#         ax_arr[2].set_title("Detection")
-#         ax_arr[0].imshow(tb.im_normalize(img_))
-#         ax_arr[1].imshow(gt_)
-#         ax_arr[2].imshow(tb.im_normalize(pred))
-#         plt.pause(0.001)
-
-
-# Separate interactive plotting of training set results
-# plt.close("all")
-# plt.ion()
-# f, ax_arr = plt.subplots(1, 3)
-# for ii, sample_batched in enumerate(trainloader):
-#     img, gt = sample_batched['image'], sample_batched['gt']
-#     inputs = img / 255 - 0.5
-#
-#     # Forward pass of the mini-batch
-#     inputs = Variable(inputs)
-#     if gpu_id >= 0:
-#         inputs = inputs.cuda()
-#
-#     output = net.forward(inputs)
-#     for jj in range(int(img.size()[0])):
-#         pred = np.transpose(output[len(output)-1].cpu().data.numpy()[jj, :, :, :], (1, 2, 0))
-#         img_ = np.transpose(img.numpy()[jj, :, :, :], (1, 2, 0))
-#         gt_ = np.transpose(gt.numpy()[jj, :, :, :], (1, 2, 0))
-#         # Plot the particular example
-#         ax_arr[0].cla()
-#         ax_arr[1].cla()
-#         ax_arr[2].cla()
-#         ax_arr[0].set_title("Input Image")
-#         ax_arr[1].set_title("Ground Truth")
-#         ax_arr[2].set_title("Detection")
-#         ax_arr[0].imshow(tb.im_normalize(img_))
-#         ax_arr[1].imshow(gt_)
-#         ax_arr[2].imshow(tb.im_normalize(pred))
-#         plt.pause(0.001)
