06338789de9bde99316fe6714bbac33633c3b05c
carlesventura <7792351+carlesventura@users.noreply.github.com>
2018-08-27 14:40:36 +0200

    Update PR_visualization.py

--- vessels/patch/evaluation/PR_visualization.py
+++ vessels/patch/evaluation/PR_visualization.py
@@ -52,29 +52,6 @@ plt.plot(recall_F_max,precision_F_max,color='blue',marker='+', ms=10)
 print([precision_F_max, recall_F_max])
 
 
-if not connected:
-    output_file = './results_dir_vessels/PR_SGH_from_DRIU.npz'
-else:
-    if from_same_vessel:
-        if bifurcations_allowed:
-            output_file = './results_dir_vessels/PR_SGH_from_DRIU_connected_same_vessel.npz'
-        else:
-            output_file = './results_dir_vessels/PR_SGH_from_DRIU_connected_same_vessel_wo_bifurcations.npz'
-    else:
-        output_file = './results_dir_vessels/PR_SGH_from_DRIU_results_connected.npz'
-
-curves = np.load(output_file)
-
-recall_overall = curves['recall_overall']
-precision_overall = curves['precision_overall']
-recall_F_max = curves['recall_F_max']
-precision_F_max = curves['precision_F_max']
-
-plt.plot(recall_overall,precision_overall, color='green', label='SHG+DRIU [F=%02f]'%(2*recall_F_max*precision_F_max/(recall_F_max+precision_F_max)))
-plt.plot(recall_F_max,precision_F_max,color='green',marker='+', ms=10)
-print([precision_F_max, recall_F_max])
-
-
 plt.ylim([0,1])
 plt.xlim([0,1])
 ax = plt.gca()
