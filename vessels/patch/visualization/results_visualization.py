176d3efb8aaeca1668514c6e08c95990443d81fb
carlesventura <7792351+carlesventura@users.noreply.github.com>
2018-08-27 13:25:05 +0200

    Update results_visualization.py

--- vessels/patch/visualization/results_visualization.py
+++ vessels/patch/visualization/results_visualization.py
@@ -14,31 +14,26 @@ num_patches = 50
 start_img = 1
 patch_size = 64
 
-#peak_th = 100
-
-gt_base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/gt_test'
-results_base_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/results'
+gt_base_dir = './results_dir_vessels/gt_test'
+results_base_dir = './results_dir_vessels/results'
 
 configs = ['_not_connected', '_connected', '_connected_same_vessel', '_connected_same_vessel_wo_bifurcations']
 
 idx_patch = 1
 num_patches = 50
 
-f = open('/scratch_net/boxy/carlesv/gt_dbs/DRIVE/vertices_selected.txt','r')
+f = open('./gt_dbs/DRIVE/vertices_selected.txt','r')
 count = 0
 
 while count != (idx_patch-1)*num_images + start_img-1:
     line = f.readline()
     count += 1
 
-
-
-#for idx in range(1,num_images+1):
 for idx in range(start_img,start_img+num_images):
 
     fig, axes = plt.subplots(5, 4)
 
-    mat_contents = sio.loadmat('/scratch_net/boxy/carlesv/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %idx)
+    mat_contents = sio.loadmat('./gt_dbs/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %idx)
     vertices = np.squeeze(mat_contents['G']['V'][0,0])-1
     subscripts = np.squeeze(mat_contents['G']['subscripts'][0,0])
     art = np.squeeze(mat_contents['G']['art'][0,0])
@@ -78,7 +73,6 @@ for idx in range(start_img,start_img+num_images):
     axes[1,3].set_ylim([patch_size-1,0])
     axes[1,3].set_aspect(1)
 
-
     for config_id in range(0,len(configs)):
 
         config_type = configs[config_id]
@@ -96,10 +90,8 @@ for idx in range(start_img,start_img+num_images):
         sources = find_peaks(pred, threshold, box_size=3)
         positions = (sources['x_peak'], sources['y_peak'])
 
-
         axes[2,config_id].imshow(pred, interpolation='nearest')
         axes[2,config_id].plot(sources['x_peak'], sources['y_peak'], ls='none', color='red',marker='+', ms=10, lw=1.5)
-
 
         gt_img = Image.open(gt_base_dir + config_type + '/img_%02d_patch_%02d_gt.png' %(idx, idx_patch))
 
@@ -111,12 +103,10 @@ for idx in range(start_img,start_img+num_images):
         else:
             gt_points = (sources_gt['x_peak'], sources_gt['y_peak'])
 
-
         for peak_th in range(1,255):
 
             valid_peaks = sources[sources['peak_value'] > peak_th]
             positions = (valid_peaks['x_peak'], valid_peaks['y_peak'])
-
 
             if len(sources_gt) > 0:
 
@@ -139,14 +129,12 @@ for idx in range(start_img,start_img+num_images):
                             true_positives += 1
 
                 if peak_th == 1:
-
                     axes[3,config_id].imshow(gt_img)
                     axes[3,config_id].plot(gt_points[0], gt_points[1], ls='none', color='green',marker='o', ms=10, lw=1.5, mfc='none')
                     axes[3,config_id].plot(positions[0], positions[1], ls='none', color='red',marker='+', ms=10, lw=1.5)
                     for i in range(0,len(row_ind)):
                         if cost[row_ind[i],col_ind[i]] < 1000:
                             axes[3,config_id].plot([positions[0][row_ind[i]], gt_points[0][col_ind[i]]], [positions[1][row_ind[i]], gt_points[1][col_ind[i]]],color='blue')
-
 
                 false_positives = total_detections - true_positives
 
@@ -162,7 +150,6 @@ for idx in range(start_img,start_img+num_images):
 
 
                 if peak_th == 254:
-                    #axes[1,1].plot(recall_all[idx-1,:],precision_all[idx-1,:])
                     axes[4,config_id].plot(recall_patch,precision_patch)
                     axes[4,config_id].set_xlim([0,1])
                     axes[4,config_id].set_ylim([0,1])
@@ -170,14 +157,12 @@ for idx in range(start_img,start_img+num_images):
                     if config_id == len(configs)-1:
                         plt.show()
 
-
             else: # len(sources_gt) = 0
                 total_detections = len(positions[0])
                 true_positives = 0
                 false_positives = total_detections - true_positives
 
                 if peak_th == 1:
-
                     axes[3,config_id].imshow(gt_img)
                     axes[3,config_id].plot(positions[0], positions[1], ls='none', color='red',marker='+', ms=10, lw=1.5)
 
@@ -192,7 +177,6 @@ for idx in range(start_img,start_img+num_images):
                 recall_patch[peak_th-1] = recall
 
                 if peak_th == 254:
-                    #axes[1,1].plot(recall_all[idx-1,:],precision_all[idx-1,:])
                     axes[4,config_id].plot(recall_patch,precision_patch)
                     axes[4,config_id].set_xlim([0,1])
                     axes[4,config_id].set_ylim([0,1])
