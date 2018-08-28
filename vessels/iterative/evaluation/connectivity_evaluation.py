c196e255a8aefda6cad5de1ca89895c34fb4d82b
carlesventura <7792351+carlesventura@users.noreply.github.com>
2018-08-28 09:25:18 +0200

    Update connectivity_evaluation.py

--- vessels/iterative/evaluation/connectivity_evaluation.py
+++ vessels/iterative/evaluation/connectivity_evaluation.py
@@ -85,11 +85,10 @@ def build_graph(pred):
 
     return G
 
-def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_connectivity, to_be_cropped):
+def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, to_be_cropped):
 
     matching_th = 0.8
-    #matching_th = 0.7
-    mat_contents = sio.loadmat('/scratch_net/boxy/carlesv/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %img_idx)
+    mat_contents = sio.loadmat('./gt_dbs/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %img_idx)
     vertices = np.squeeze(mat_contents['G']['V'][0,0])-1
     connected = 0
     edges_gt_inside_roi = 0
@@ -118,8 +117,6 @@ def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_conne
                 for kk in range(0,len(line)):
                     if line[kk][0] < 27 or line[kk][0] > 538 or line[kk][1] < 37 or line[kk][1] > 548:
                         inside_roi = False
-                #if not inside_roi:
-                    #print(np.array([ii, line[0][0], line[0][1] ]))
 
         if to_be_cropped and inside_roi:
 
@@ -178,14 +175,6 @@ def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_conne
             if connectivity > matching_th:
                 connected += 1
             if connectivity <= matching_th and visualize_errors:
-            #if visualize_errors:
-                #print(length_pred)
-                #print(length_gt)
-                #print(float(np.min([length_gt,length_pred]))/np.max([length_gt,length_pred]))
-
-                #fig, axes = plt.subplots(2, 2)
-                #axes[0,0].imshow(img)
-                #axes[0,1].imshow(pred)
 
                 plt.imshow(pred)
                 pos_y_vector = []
@@ -195,13 +184,6 @@ def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_conne
                     pos_x = path_pred[jj] % pred.shape[1]
                     pos_y_vector.append(pos_y)
                     pos_x_vector.append(pos_x)
-
-                #plt.scatter(pos_x_vector,pos_y_vector,color='red',marker='o')
-
-                #axes[1,0].imshow(img)
-                #axes[1,1].imshow(pred)
-                #axes[1,0].scatter(pos_x_vector,pos_y_vector,color='red',marker='o')
-                #axes[1,1].scatter(pos_x_vector,pos_y_vector,color='red',marker='o')
 
                 pos_y_vector = []
                 pos_x_vector = []
@@ -214,56 +196,10 @@ def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_conne
                 plt.scatter(pos_x_vector[0],pos_y_vector[0],color='red',marker='o')
                 plt.scatter(pos_x_vector[-1],pos_y_vector[-1],color='red',marker='o')
                 plt.scatter(pos_x_vector,pos_y_vector,color='red',marker='+')
-                #axes[1,0].scatter(pos_x_vector,pos_y_vector,color='green',marker='+')
-                #axes[1,1].scatter(pos_x_vector,pos_y_vector,color='green',marker='+')
-                #plt.show()
-
-
-
-            if visualize_connectivity and ii%10 == 0:
-
-                print(length_pred)
-                print(length_gt)
-                print(float(np.min([length_gt,length_pred]))/np.max([length_gt,length_pred]))
-
-                #plt.imshow(img)
-                #plt.imshow(pred)
-
-                pred_img = np.zeros((584,565,3),dtype=np.uint8)
-                no_pred_indxs = np.argwhere(pred==0)
-                pred_indxs = np.argwhere(pred>0)
-                pred_img[no_pred_indxs[:,0],no_pred_indxs[:,1],:]=255
-                pred_img[pred_indxs[:,0],pred_indxs[:,1],1:]=255
-                plt.imshow(pred_img)
-
-                pos_y_vector = []
-                pos_x_vector = []
-                for jj in range(0,len(path_gt)):
-                    pos_y = path_gt[jj] / pred.shape[1]
-                    pos_x = path_gt[jj] % pred.shape[1]
-                    pos_y_vector.append(pos_y)
-                    pos_x_vector.append(pos_x)
-
-                marker = mmarkers.MarkerStyle(marker='o', fillstyle='none')
-                plt.scatter(pos_x_vector,pos_y_vector, color='green', facecolors='none',s=100,linewidth=2)
-                pos_y_vector = []
-                pos_x_vector = []
-                for jj in range(0,len(path_pred)):
-                    pos_y = path_pred[jj] / pred.shape[1]
-                    pos_x = path_pred[jj] % pred.shape[1]
-                    pos_y_vector.append(pos_y)
-                    pos_x_vector.append(pos_x)
-
-                #plt.scatter(pos_x_vector,pos_y_vector,color='red',marker='o')
-                plt.scatter(pos_x_vector,pos_y_vector,color='red',marker='+',s=100,linewidth=2)
-
-                plt.show()
 
     plt.show()
 
     if to_be_cropped:
-        print(edges_gt_inside_roi)
-        print(len(edges_gt))
         CRR = float(connected) / edges_gt_inside_roi
     else:
         CRR = float(connected) / len(edges_gt)
@@ -272,7 +208,7 @@ def evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_conne
 
 def extract_edges_from_gt_annotations(img_idx):
 
-    mat_contents = sio.loadmat('/scratch_net/boxy/carlesv/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %img_idx)
+    mat_contents = sio.loadmat('./gt_dbs/artery-vein/AV-DRIVE/test/%02d_manual1.mat' %img_idx)
     subscripts = np.squeeze(mat_contents['G']['subscripts'][0,0])-1
     junctions = np.squeeze(mat_contents['G']['junctions'][0,0])-1
     edges_gt = []
@@ -339,36 +275,12 @@ def extract_edges_from_gt_annotations(img_idx):
     return edges_gt, graph_gt_img
 
 
-visualize_connectivity = True
 visualize_errors = False
 
-GT_vessel_segmentation = False
-DRIU_vessel_supervision = False
-DRIU_point_supervision = False
-iterative_graph_creation = True
-
-
-root_dir = '/scratch_net/boxy/carlesv/gt_dbs/DRIVE/'
-
-if GT_vessel_segmentation:
-    results_dir = root_dir + 'test/1st_manual/'
-    to_be_cropped = False
-    skeletonized = True
-elif DRIU_vessel_supervision:
-    results_dir = '/scratch_net/boxy/carlesv/HourGlasses_experiments/Iterative_margin_6/results_DRIU_vessel_segmentation/'
-    skeletonized = False
-    to_be_cropped = False
-elif DRIU_point_supervision:
-    results_dir = '/scratch/carlesv/results/DRIVE/Results_DRIU_point_supervision_skeleton/pred_th_130/'
-    skeletonized = True
-    to_be_cropped = True
-else: #iterative
-    #results_dir = '/scratch/carlesv/results/DRIVE/Results_iterative_graph_creation/'
-    #results_dir = '/scratch/carlesv/results/DRIVE/Results_iterative_graph_creation_th_25/'
-    #results_dir = '/scratch/carlesv/results/DRIVE/Results_iterative_graph_creation_no_mask_offset/'
-    results_dir = '/scratch/carlesv/results/DRIVE/Results_iterative_graph_creation_no_mask_offset_th_25/'
-    to_be_cropped = False
-    skeletonized = True
+root_dir = './gt_dbs/DRIVE/'
+results_dir = './results_dir_vessels/results_iterative_graph_creation_no_mask_offset_th_25/'
+to_be_cropped = False
+skeletonized = True
 
 CRR_all = []
 
@@ -382,35 +294,11 @@ for img_idx in range(1,21):
 
     #Predicted graph with DRIU
 
-    if not GT_vessel_segmentation:
-
-        if DRIU_point_supervision:
-            pred = Image.open(results_dir + 'pred_graph_%02d.png' %(img_idx))
-        elif iterative_graph_creation:
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20_confidence_th_040.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20_confidence_th_040_skeleton.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20_confidence_th_040_2nd_step.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20_confidence_th_100_2nd_step_extended_branches.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_postprocessed_minval_10_maxdepth_20_confidence_th_100_2nd_step_extended_branches_wo_not_connected.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_mask_graph_dilated_skeleton.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_mask_graph_dilated_skeleton_extended_branches_confidence_th_100.png' %(img_idx))
-            #pred = Image.open(results_dir + 'pred_graph_%02d_mask_graph_offset_2_dilated_skeleton.png' %(img_idx))
-            pred = Image.open(results_dir + 'pred_graph_%02d_mask_graph_offset_2_dilated_skeleton_extended_branches_confidence_th_100.png' %(img_idx))
-        else: #DRIU vessel supervision
-            pred = Image.open(results_dir + '%02d_test.png' %(img_idx))
-
-        pred = np.array(pred)
-        if DRIU_vessel_supervision:
-            pred = skeletonize(pred>170)
-
-        G_pred = build_graph(pred)
-
-        print('Predicted graph built')
-
+    pred = Image.open(results_dir + 'pred_graph_%02d_mask_graph_extended_branches.png' %(img_idx))
+    pred = np.array(pred)
+    G_pred = build_graph(pred)
+    
+    print('Predicted graph built')
 
     #Ground truth graph (Estrada annotations)
 
@@ -419,22 +307,8 @@ for img_idx in range(1,21):
 
     print('GT graph built')
 
-    #Ground truth graph from ground truth vessel segmentation
-
-    if GT_vessel_segmentation:
-
-        #results_dir_gt = root_dir + 'test/1st_manual/'
-        pred = Image.open(results_dir + '%02d_manual1.gif' %(img_idx))
-        pred = np.array(pred)
-        pred = skeletonize(pred>200)
-
-        G_pred = build_graph(pred)
-
-        print('GT from vessel segmentation graph built')
-
-
     # Find matching between predicted edges and ground truth edges
-    CRR = evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, visualize_connectivity, to_be_cropped)
+    CRR = evaluate_connectivity(img_idx, edges_gt, G_gt, pred, G_pred, to_be_cropped)
 
     print(CRR)
 
@@ -443,10 +317,4 @@ for img_idx in range(1,21):
 
 print(CRR_all)
 print(np.mean(CRR_all))
-
-
-
-
-
-
 
